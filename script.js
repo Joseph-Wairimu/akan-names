01ce7bfc06b94806c1672167392b35614751c41e
Joseph-Wairimu <josephkamore084@gmail.com>
2021-12-11 18:00:23 +0300

    input an array with male names

---
+++ script.js
@@ -0,0 +1,9 @@
+let akanMale = [
+    { day: "Sunday", Name: "Kwasi" },
+    { day: "Monday", Name: "Kwadwo" },
+    { day: "Tuesday", Name: "Kwabena" },
+    { day: "Wednesday",Name: "Kwaku" },
+    { day: "Thursday", Name: "Yaw" },
+    { day: "Friday", Name: "Kofi" },
+    { day: "Saturday", Name: "Kwame" },
+];
