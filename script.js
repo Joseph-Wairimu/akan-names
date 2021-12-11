9a17c31b7f318db77471d7e97f49a7a8303d6f15
Joseph-Wairimu <josephkamore084@gmail.com>
2021-12-11 18:02:48 +0300

    input array with female akan names

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let akanMale = [
+let Male = [
     { day: "Sunday", Name: "Kwasi" },
     { day: "Monday", Name: "Kwadwo" },
     { day: "Tuesday", Name: "Kwabena" },
@@ -6,4 +6,14 @@ let akanMale = [
     { day: "Thursday", Name: "Yaw" },
     { day: "Friday", Name: "Kofi" },
     { day: "Saturday", Name: "Kwame" },
+];
+
+let Female = [
+    { day: "Sunday", Name: "Akosua" },
+    { day: "Monday", Name: "Adwoa" },
+    { day: "Tuesday", Name: "Abenaa" },
+    { day: "Wednesday", Name: "Akua" },
+    { day: "Thursday", Name: "Yaa" },
+    { day: "Friday", Name: "Afua" },
+    { day: "Saturday", Name: "Ama" },
 ];
